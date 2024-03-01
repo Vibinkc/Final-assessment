1311415a0d59271a69cefb6c824c8754394ed294
Vibinkc <107981540+Vibinkc@users.noreply.github.com>
2024-03-01 11:04:16 +0530

    Update login.js

--- WIZARD HOUSE/login.js
+++ WIZARD HOUSE/login.js
@@ -8,7 +8,6 @@ function validation(){
     if(input_text.value.length <= 6 || input_password.value.length <= 6 ){
       error_msg.style.display = "inline-block";
       input_text.style.border = "1px solid #f74040";
-      return false;
     }
     else{
       return true;
